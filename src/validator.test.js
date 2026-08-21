ef2f037d64287441289758cb3de79582798b622c
Jose Claudio de Sousa <jcsousa@pf.cruzeirodosul.edu.br>
2026-08-21 19:59:43 -0300

    Commit inicial

---
+++ src/validator.test.js
@@ -0,0 +1,29 @@
+const validarChamado = require("./validator");
+
+describe("Suíte de Testes - Validador de Chamados", () => {
+
+    test("Deve validar com sucesso um chamado com título preenchido", () => {
+        const resultado = validarChamado(
+            "Erro de conexão com o banco MySQL"
+        );
+
+        expect(resultado).toBe(true);
+    });
+
+    test("Deve lançar erro se o título do chamado for vazio", () => {
+        expect(() => {
+            validarChamado("");
+        }).toThrow(
+            "O título do chamado não pode ser vazio."
+        );
+    });
+
+    test("Deve lançar erro se o título for composto apenas por espaços em branco", () => {
+        expect(() => {
+            validarChamado("   ");
+        }).toThrow(
+            "O título do chamado não pode ser vazio."
+        );
+    });
+
+});
